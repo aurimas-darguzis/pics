40f59fbf865aac60d32af106dd7d15afb01d91b3
Aurimas <aurimas@darguzis.com>
2019-02-04 22:22:24 +0000

    added list of images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
 const ImageList = (props) => {
+  const images = props.images.map(({description, id, urls}) => {
+    return <img key={id}  src={urls.regular} alt={description} />
+  })
   return (
-    <div>Image List</div>
+    <div>{images}</div>
     )
 }
 
